f982d970b7f9401f36df91b1e6481a7e1fcfaaff
Anders Modén <anders@tooltech-software.com>
2018-11-12 20:49:02 +0100

    Added a simple message client as clean as possibles

--- Distributed_Domain_Demo/Program.cs
+++ Distributed_Domain_Demo/Program.cs
@@ -11,9 +11,53 @@ namespace Distributed_Domain_Demo
 {
     class Program
     {
+       
+
         static void Main(string[] args)
         {
+            GizmoSDK.GizmoBase.Platform.Initialize();
+
+            GizmoSDK.GizmoDistribution.Platform.Initialize();
+
             DistManager manager = DistManager.GetManager(true);
+
+            
+            manager.Start(DistRemoteChannel.CreateDefaultSessionChannel(), DistRemoteChannel.CreateDefaultServerChannel());
+
+            DistClient client = new DistClient("MessageClient", manager);
+
+            client.Initialize();
+
+            DistSession session=client.GetSession("MessageSession", true, true);
+
+            client.JoinSession(session);
+
+            client.SubscribeEvents(session);
+
+            client.OnEvent += Client_OnEvent;
+
+            while(true)
+            {
+                string result = Console.ReadLine();
+
+                if (result == "quit")
+                    break;
+
+                DistEvent e = manager.GetEvent();
+
+                e.SetAttributeValue("Message", result);
+
+                client.SendEvent(e, session);
+            }
+
+
+            manager.Shutdown();
+        }
+
+        private static void Client_OnEvent(DistClient sender, DistEvent e)
+        {
+            if(e.GetSource()!=sender.GetClientID().InstanceID)
+                Console.WriteLine(e.GetAttributeValue("Message").GetString());
         }
     }
 }
