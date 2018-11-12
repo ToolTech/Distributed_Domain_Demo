30424002329cb827650ce6c4e3ef2d79b45a5be7
Anders Modén <anders@tooltech-software.com>
2018-11-12 20:05:29 +0100

    Added files

---
+++ Distributed_Domain_Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GizmoSDK.GizmoBase;
+using GizmoSDK.GizmoDistribution;
+
+namespace Distributed_Domain_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DistManager manager = DistManager.GetManager(true);
+        }
+    }
+}
---
+++ junction_vs15_release.bat
@@ -0,0 +1,26 @@
+rem ---- DONT RUN THIS as a GIT USER, No need --------------------------------
+rem ---- Set up all paths in the project as relative paths - Saab Dev Only ---
+
+
+rem ------------- GizmoSDK dependencies --------------------------------------
+
+if not defined GIZMOSDK (
+  set GIZMOSDK=..\GizmoSDK
+)
+
+rmdir /S / Q Assets\Saab\GizmoSDK
+mkdir Assets\Saab\GizmoSDK
+
+mklink /J Assets\Saab\GizmoSDK\GizmoBase %GIZMOSDK%\GizmoBase\source\C#
+mklink /J Assets\Saab\GizmoSDK\GizmoDistribution %GIZMOSDK%\GizmoDistribution\source\C#
+
+
+rem ------------- End of GizmoSDK dependencies -------------------------------
+
+
+rem ------------- runtime dlls -----------------------------------------------
+
+rmdir /S / Q Assets\Plugins
+mkdir Assets\Plugins
+
+mklink /J Assets\Plugins\x64 %GIZMOSDK%\GizmoDistribution\ws\C#\vs15\GizmoDistribution\x64\Release
