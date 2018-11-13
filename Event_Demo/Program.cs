e1853d695cf2dd85e8bf5bfd49f95caf587ce9a3
Anders Modén <anders.moden@saabgroup.com>
2018-11-13 13:28:33 +0100

    Added some comments

--- Event_Demo/Program.cs
+++ Event_Demo/Program.cs
@@ -15,28 +15,36 @@ namespace Event_Demo
 
         static void Main(string[] args)
         {
+            // Initialize platforms for various used SDKs
             GizmoSDK.GizmoBase.Platform.Initialize();
-
             GizmoSDK.GizmoDistribution.Platform.Initialize();
 
            
-
+            // Create a manager. The manager controls it all
             DistManager manager = DistManager.GetManager(true);
 
-            
+            // Start the manager with settting for transport protocols
             manager.Start(DistRemoteChannel.CreateDefaultSessionChannel(), DistRemoteChannel.CreateDefaultServerChannel());
 
+
+            // Client set up. You are a client that sends and receives information
             DistClient client = new DistClient("MessageClient", manager);
 
+            // We need to tell the client how to initialize
             client.Initialize();
 
+            // Now we can get a session. A kind of a meeting room that is used to exchange various "topics"
             DistSession session=client.GetSession("MessageSession", true, true);
 
+            // Joint that session and subribe all events
             client.JoinSession(session);
-
             client.SubscribeEvents(session);
 
+            // Create a delegete
             client.OnEvent += Client_OnEvent;
+
+
+            // Now loops around some simple program to get strings from console and distribute them as a message app
 
             while(true)
             {
@@ -45,15 +53,21 @@ namespace Event_Demo
                 if (result == "quit")
                     break;
 
+                // get a new empty event from manager
                 DistEvent e = manager.GetEvent();
 
+                // set some attributes in the event to any kind of value
                 e.SetAttributeValue("Message", result);
 
+                // and send the event on the specific session
                 client.SendEvent(e, session);
             }
 
-
+            // Some kind of graceful shutdown
             manager.Shutdown();
+
+
+            // GC and platform uninit is managed by the system automatically
         }
                 
 
